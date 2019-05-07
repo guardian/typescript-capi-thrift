18941a89cf2b8df6bb13273372a0f15ecac83fbb
Alex Ware <alex.ware@guardian.co.uk>
2019-05-07 15:22:26 +0100

    Ready to package

--- index.ts
+++ index.ts
@@ -1,22 +1,41 @@
-console.log("HELLO")
-import { generate } from '@creditkarma/thrift-typescript'
+import { generate } from "@creditkarma/thrift-typescript";
 
-console.log(process.cwd())
+console.log(process.cwd());
 // Generates TypeScript and saves to given outDir
 generate({
-    rootDir: process.cwd(),
-    sourceDir: 'thrift',
-    outDir: 'codegen',
-    target: 'thrift-server',
-    files: [
-        'v1.thrift',
-        'story_model.thrift',
-        'contentatom.thrift',
-        'atoms/audio.thrift','atoms/cta.thrift','atoms/interactive.thrift','atoms/qanda.thrift','atoms/review.thrift',
-        'atoms/chart.thrift','atoms/explainer.thrift','atoms/media.thrift','atoms/quiz.thrift','atoms/storyquestions.thrift',
-        'atoms/commonsdivision.thrift','atoms/guide.thrift','atoms/profile.thrift','atoms/recipe.thrift','atoms/timeline.thrift',
-        'atoms/shared.thrift','entity.thrift','entities/film.thrift','entities/organisation.thrift','entities/place.thrift','entities/shared.thrift','entities/game.thrift','entities/person.thrift','entities/restaurant.thrift',
-        'story_package_article.thrift',
-    ],
-    fallbackNamespace: 'java',
-})
+  rootDir: process.cwd(),
+  sourceDir: "thrift",
+  outDir: "capi-ts/src",
+  target: "thrift-server",
+  files: [
+    "v1.thrift",
+    "story_model.thrift",
+    "contentatom.thrift",
+    "atoms/audio.thrift",
+    "atoms/cta.thrift",
+    "atoms/interactive.thrift",
+    "atoms/qanda.thrift",
+    "atoms/review.thrift",
+    "atoms/chart.thrift",
+    "atoms/explainer.thrift",
+    "atoms/media.thrift",
+    "atoms/quiz.thrift",
+    "atoms/storyquestions.thrift",
+    "atoms/commonsdivision.thrift",
+    "atoms/guide.thrift",
+    "atoms/profile.thrift",
+    "atoms/recipe.thrift",
+    "atoms/timeline.thrift",
+    "atoms/shared.thrift",
+    "entity.thrift",
+    "entities/film.thrift",
+    "entities/organisation.thrift",
+    "entities/place.thrift",
+    "entities/shared.thrift",
+    "entities/game.thrift",
+    "entities/person.thrift",
+    "entities/restaurant.thrift",
+    "story_package_article.thrift"
+  ],
+  fallbackNamespace: "java"
+});
